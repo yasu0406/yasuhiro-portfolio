cdadeee55789083ed4af333d01cc5a95f16655c4
yasuhiro <yasucom4646@gmail.com>
2020-02-13 17:38:24 -0800

    Create animatedMouse.component.jsx

---
+++ src/components/animatedMouse/animatedMouse.component.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { TweenMax } from 'gsap';
+import { Cursor, Follower } from './animatedMouse.style';
+
+var mouseX = 0,
+    mouseY = 0,
+    posX = 0,
+    posY = 0;
+const AnimatedMouse = () => {
+    const cursor = useRef(null);
+    const follower = useRef(null);
+    const [mouseToggle, setMouseToggle] = useState(false);
+    const [animatedComp] = useState({
+        cWidth:8,
+        fWidth:40,
+        delay:40
+    });
+    useEffect(() => {
+        window.addEventListener('mousemove', onMouseMove);
+        mouseAction();
+        return () => {
+            window.removeEventListener('mousemove', onMouseMove);
+            setMouseToggle(false);
+          };
+    },[]);
+    const onMouseMove = e => {
+        mouseX = e.pageX;
+        mouseY = e.pageY;
+        TweenMax.to({}, 1, {
+            repeat: -1,
+            onRepeat: function() {
+                posX += (mouseX - posX) / animatedComp.delay;
+                posY += (mouseY - posY) / animatedComp.delay;
+                TweenMax.set(follower.current, {
+                    css: {    
+                        left: posX - (animatedComp.fWidth / 2),
+                        top: posY - (animatedComp.fWidth / 2)
+                    }
+                });
+                TweenMax.set(cursor.current, {
+                    css: {    
+                        left: mouseX - (animatedComp.cWidth / 2),
+                        top: mouseY - (animatedComp.cWidth / 2)
+                    }
+                });
+            }
+          });
+      };
+      const mouseAction = () => {
+        var hoverLink = document.querySelectorAll('a');
+        Array.from(hoverLink).forEach(link => {
+          link.addEventListener('mouseenter', () => {
+            setMouseToggle(true);
+          });
+          link.addEventListener('mouseleave', () => {
+            setMouseToggle(false);
+          });
+      });
+      }
+    return (
+        <>
+            <Cursor className={`${mouseToggle? 'is-active' : '' } cursor`} ref={cursor} />
+            <Follower className={`${mouseToggle? 'is-active' : '' } follower`} ref={follower} />
+        </>
+    )
+}
+
+export default AnimatedMouse;
