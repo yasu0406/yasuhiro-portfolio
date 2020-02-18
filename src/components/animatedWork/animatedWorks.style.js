76b0fedcc0e2cbb9b3de05d3863b14f37bb46a72
yasuhiro <yasucom4646@gmail.com>
2020-02-17 21:45:53 -0800

    Create animatedWorks.style.js

---
+++ src/components/animatedWork/animatedWorks.style.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+import { colors } from '../../styles/variable';
+
+export const MtAnimation = styled.div`
+    width: 100%;
+    max-width:600px;
+    max-height:600px;
+    border-radius:100%;
+    position:fixed;
+    top:0;
+    left:0;
+    right:0;
+    bottom:0;
+    margin:auto;
+    background-color: ${colors.mainRedColor};
+    z-index:100;
+    overflow:hidden;
+    h1 {
+        height:10px;
+        font-size:1rem;
+        text-align:center;
+        position:absolute;
+        top:0;
+        left:0;
+        right:0;
+        bottom:0;
+        margin:auto;
+        color: ${colors.colorWh};
+        opacity:1;
+    }
+`;
+
+export const FuhiImg = styled.img`
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    margin: auto;
+    z-index: 101;
+    opacity:0;
+`;
