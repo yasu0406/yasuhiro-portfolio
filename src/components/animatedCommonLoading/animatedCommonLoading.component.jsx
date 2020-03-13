3b2f67e8cc0c8fc7d8d049c0560adba48da6561c
yasuhiro <yasucom4646@gmail.com>
2020-03-12 20:05:34 -0700

    Create animatedCommonLoading.component.jsx

---
+++ src/components/animatedCommonLoading/animatedCommonLoading.component.jsx
@@ -0,0 +1,46 @@
+import React, { useRef, useEffect } from 'react';
+import { Route } from 'react-router-dom';
+import { TweenLite, Expo } from 'gsap';
+import { MtAnimation, FuhiImg } from './animatedCommonLoading.style';
+
+import ProfileComponent from '../profile/profile.component';
+import PreviewWorks from '../preview-works/preview-works.component';
+import WorkDetailComponent from '../work-detail/work-detail.component';
+
+import fuji from '../../images/fuji.svg';
+
+const AnimatedWorks = (props) => {
+    const circleRed = useRef(null);
+    const title = useRef(null);
+    const fujiImage = useRef(null); 
+    useEffect(() => {
+        TweenLite.set(circleRed.current, { scale: 0 });
+        TweenLite.to(circleRed.current, 2.5, {
+            delay: 1.5,
+            scale: 1,
+            zIndex: 0,
+            ease: Expo.easeInOut
+          });
+
+          TweenLite.to(title.current, 3, {
+            delay: 1.7,
+            height:'200px',
+            fontSize: '15.2rem',
+            scale: 10,
+            ease: Expo.easeInOut,
+            opacity: 0
+          });        
+    })
+    return(
+        <>
+        <MtAnimation ref={circleRed}>
+          <h1 ref={title}>{props.title}</h1>
+        </MtAnimation>
+        <Route exact path='/profile' component={() => <ProfileComponent circleRed={circleRed} />} />
+        <Route exact path='/works' component={() => <PreviewWorks fuji={<FuhiImg ref={fujiImage} src={fuji} />} circleRed={circleRed} />} />
+        <Route exact path='/works/:title' component={() => <WorkDetailComponent circleRed={circleRed} />} />
+        </>
+    )
+};
+
+export default AnimatedWorks;
