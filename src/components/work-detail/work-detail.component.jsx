7418f53ae7ee9d476d9ab5b5180e1e14132c9aec
yasuhiro <yasucom4646@gmail.com>
2020-03-23 04:16:55 -0700

    Updated work-detail for animation

--- src/components/work-detail/work-detail.component.jsx
+++ src/components/work-detail/work-detail.component.jsx
@@ -1,157 +1,166 @@
-import React, {
-    useEffect,
-    useRef
-} from 'react';
-import { Link } from 'react-router-dom';
-import { TweenLite, Linear, Expo } from 'gsap';
+import React, { useEffect, useRef, useContext } from 'react';
+import { useHistory } from "react-router-dom";
+import { TweenLite, Expo } from 'gsap';
 import ScrollMagic from 'scrollmagic';
+import ProjectsContext from '../../context/projects/projects.context';
 
-import { InnerConatiner, MainInnerConatiner, FirstConatiner, WorkImg, SecondConatiner, WorkDescription, FooterNav, WorkButton} from './work-detail.style';
-import { Title, Text } from '../../styles/base.style';
+import { Title, Text, ScrollDown } from '../../styles/base.style';
+import { InnerConatiner, MainInnerConatiner, FirstConatiner, WorkImg, SecondConatiner, ProjectImgDiv, FooterNav} from './work-detail.style';
 
-import workD from '../../images/work-detail.png';
-import workD02 from '../../images/work-detail02.png';
-import workButton from '../../images/workButton.svg';
-
-const WorkDetailComponent = ({circleRed}) => {
+const WorkDetailComponent = (props) => {
+    const projects = useContext(ProjectsContext);
+    const controller = new ScrollMagic.Controller();
+    const projectDetail = projects.filter(project => project.url === props.match.params.title);
+    const {id, title, description, imgUrl, bgColor, viewLink, projectImg01, projectImg02, projectImg03, projectImg04, date, develop, project, useProgram, appleStore, googlePlay} = projectDetail[0];
+    const prevId = id !== 1 ? id - 1 : null;
+    const nextId = id !== projects.length ? id + 1 : null;
+    const prevUrl = projects.filter(project => project.id === prevId);
+    const nextUrl = projects.filter(project => project.id === nextId);
     const firstContainer = useRef(null);
     const secondContainer = useRef(null);
     const thirdContainer = useRef(null);
     const mainTitle = useRef(null);
     const mainText = useRef(null);
-    const viewText = useRef(null);
-    const viewButton = useRef(null);
     const mainWorkImg = useRef(null);
-    const secondWorkImg = useRef(null);
-    const thirdWorkImg = useRef(null);
+    const projectDescription = useRef(null);
+    const footerNav = useRef(null);
+    const scrollDwon = useRef(null);
     let category = [];
     let footerLinks = [];
-    const workImg = useRef(null);
-    const controller = new ScrollMagic.Controller();
+    let projectsImg = [];
+    let projectsImgDiv = [];
+    let history = useHistory();
     
     useEffect(() => {
         window.addEventListener('scroll', handleScroll);
-        TweenLite.to(workImg.current, 10,{rotation:"360", ease:Linear.easeNone, repeat:-1});
         mainAnimation();
         new ScrollMagic.Scene({
             triggerElement: secondContainer.current
+            }).on("enter", function (event) {
+                TweenLite.to(category[0], 0.5, {
+                    delay:0.3,
+                    transform: "translateY(0)",
+        
+                });
+                TweenLite.to(category[1], 0.5, {
+                    delay:0.5,
+                    transform: "translateY(0)",
+        
+                });
+                TweenLite.to(category[2], 0.5, {
+                    delay:0.8,
+                    transform: "translateY(0)",
+        
+                });
+                TweenLite.to(category[3], 0.5, {
+                    delay:1.1,
+                    transform: "translateY(0)",
+        
+                });
+                TweenLite.to(category[4], 0.5, {
+                    delay:1.4,
+                    transform: "translateY(0)",
+                });
+                TweenLite.to(projectDescription.current, 0.5, {
+                    transform: "translateY(0)",
+                });
+            }).on("leave", function (event) {
+                category.map(cate => {
+                    TweenLite.to(cate, 0.5, {
+                        transform: "translateY(100%)",
+                    });
+                })
+                TweenLite.to(projectDescription.current, 0.5, {
+                    transform: "translateY(100%)",
+                });
             }).setClassToggle(secondContainer.current, "active").addTo(controller);
-            new ScrollMagic.Scene({
-                triggerElement: thirdContainer.current
-            }).setClassToggle(thirdContainer.current, "active").addTo(controller);
-            mouseOn();
-            mouseOut();
+
+            projectsImgDiv.map(projectImgDiv => {
+                new ScrollMagic.Scene({
+                    triggerElement: projectImgDiv
+                }).setClassToggle(projectImgDiv, "active")
+                .on("enter", function (event) {
+                    projectEnterAnimation();
+                })
+                .on("leave", function (event) {
+                    projectLeaveAnimation();
+            }).addTo(controller);
+            });
         return()=>{
             window.removeEventListener('scroll', handleScroll);
+            window.removeEventListener('mouseenter', mouseOn);
+            window.removeEventListener('mouseleave' ,mouseOut);
         }
     }, []);
 
+    const changePage = (e, destination) => {
+        e.preventDefault();
+        TweenLite.to(footerLinks, 1, {
+            width: '50vw'
+        });
+        TweenLite.to(footerNav.current.querySelector('ul'), 0.5, {
+            opacity:0
+        });
+        TweenLite.to(footerNav.current, 0.5, {
+            delay:0.5,
+            scale:10
+        });
+        TweenLite.to(e.target.querySelector('span'), 1, {
+            opacity: 0
+        });
+        setTimeout(() => {
+          history.push(destination);
+        }, 1500);
+    };
+
     const mainAnimation = () => {
-          TweenLite.to(circleRed.current, 2, {
+        TweenLite.to(props.circleRed.current, 2, {
             delay: 4,
             transform: 'scale(0.8)',
             zIndex:'-5',
             ease: Expo.easeInOut
         });
+        TweenLite.to(scrollDwon.current, 1, {
+            delay:5,
+            opacity: 1,
+        });
         TweenLite.to(mainTitle.current, 0.5, {
             delay:5,
             transform: "translateY(0%)",
-            ease:Linear.easeNone
         });
         TweenLite.to(mainText.current, 0.5, {
             delay:5.2,
             transform: "translateY(0%)",
-            ease:Linear.easeNone
         });
-        TweenLite.to(viewButton.current, 1, {
-            delay:5.5,
-            opacity: 1,
-            ease:Linear.easeNone
-        });
-        TweenLite.to(viewText.current, 0.5, {
-            delay:5.5,
-            transform: "translateY(0%)",
-            ease:Linear.easeNone
-        });
-        TweenLite.to(mainWorkImg.current, 1, {
+        TweenLite.to(mainWorkImg.current, 1.5, {
             delay:6.5,
-            height: '55rem',
-            ease:Linear.easeNone
+            height: 'auto',
+            ease: Expo.easeInOut
         });
     }
 
+    const projectEnterAnimation = () => {
+        projectsImgDiv.filter(projectImgDiv => projectImgDiv.classList.contains('active')).map(projectImgDiv => {
+            TweenLite.to(projectImgDiv.querySelector('div'), 1, {
+                width: "0",
+                ease: Expo.easeInOut
+            });
+        });
+    }
+    const projectLeaveAnimation = () => {
+        projectsImgDiv.filter(projectImgDiv => !projectImgDiv.classList.contains('active')).map(projectImgDiv => {
+            TweenLite.to(projectImgDiv.querySelector('div'), 1, {
+                width: "100%"
+            });
+        });
+    }
     const handleScroll = () => {
         var y = window.pageYOffset;
         if(0 < y) {
-            TweenLite.to(firstContainer.current, 0, {
-                zIndex:'-1',
+            TweenLite.to(firstContainer.current, {
                 position: 'fixed'
             });
-        } else {
-            TweenLite.to(firstContainer.current, 1, {
-                zIndex:'1'
-            });
         }
-        if(secondContainer.current.classList.contains('active')) {
-            TweenLite.to(category[0], 0.5, {
-                delay:0.3,
-                transform: "translateY(0%)",
-                ease:Linear.easeNone
-            });
-            TweenLite.to(category[1], 0.5, {
-                delay:0.5,
-                transform: "translateY(0%)",
-                ease:Linear.easeNone
-            });
-            TweenLite.to(category[2], 0.5, {
-                delay:0.8,
-                transform: "translateY(0%)",
-                ease:Linear.easeNone
-            });
-            TweenLite.to(category[3], 0.5, {
-                delay:1.1,
-                transform: "translateY(0%)",
-                ease:Linear.easeNone
-            });
-            TweenLite.to(category[4], 0.5, {
-                transform: "translateY(0%)",
-                ease:Linear.easeNone
-            });
-            TweenLite.to(secondWorkImg.current, 0.5, {
-                transform: "translateY(0%)",
-                ease:Linear.easeNone
-            });
-        } else {
-            TweenLite.to(category[0], 0.5, {
-                transform: "translateY(100%)",
-            });
-            TweenLite.to(category[1], 0.5, {
-                transform: "translateY(100%)",
-            });
-            TweenLite.to(category[2], 0.5, {
-                transform: "translateY(100%)",
-            });
-            TweenLite.to(category[3], 0.5, {
-                transform: "translateY(100%)",
-            });
-            TweenLite.to(category[4], 0.5, {
-                transform: "translateY(100%)",
-            });
-            TweenLite.to(secondWorkImg.current, 0.5, {
-                transform: "translateY(100%)",
-                ease:Linear.easeNone
-            });
-        };
-        if(thirdContainer.current.classList.contains('active')) {
-            TweenLite.to(thirdWorkImg.current, 1, {
-                transform: "translateY(0%)",
-                ease:Linear.easeNone
-            });
-        } else {
-            TweenLite.to(thirdWorkImg.current, 0.5, {
-                transform: "translateY(100%)",
-            });
-        };
       };
       const mouseOn = () => {
         for(var i = 0; i < footerLinks.length; i++) {
@@ -170,7 +179,6 @@ const WorkDetailComponent = ({circleRed}) => {
             });
         }
     };
-
     const mouseOut = () => {
         for(var i = 0; i < footerLinks.length; i++) {
             footerLinks[i].addEventListener('mouseleave', e => {
@@ -183,45 +191,70 @@ const WorkDetailComponent = ({circleRed}) => {
             });
         }
     };
+    const pageNation = () => {
+        if(prevUrl.length === 0) {
+            return (
+                <ul>
+                    <li onMouseEnter={mouseOn.bind(this)} onMouseLeave={mouseOut.bind(this)} className='active-link' ref={(e) => {e !== null && footerLinks.push(e)}}><a onClick={e => changePage(e, `/works/${nextUrl[0].url}`)}>next</a><span>next</span></li>
+                </ul>
+            )
+        } else if(nextUrl.length === 0) {
+            return (
+                <ul>
+                    <li onMouseEnter={mouseOn.bind(this)} onMouseLeave={mouseOut.bind(this)} className='active-link' ref={(e) => {e !== null && footerLinks.push(e)}}><a onClick={e => changePage(e, `/works/${prevUrl[0].url}`)}>prev</a><span>prev</span></li>
+                </ul>
+            )
+        } else {
+            return (
+                <ul>
+                    <li onMouseEnter={mouseOn.bind(this)} onMouseLeave={mouseOut.bind(this)} ref={(e) => {e !== null && footerLinks.push(e)}}><a onClick={e => changePage(e, `/works/${prevUrl[0].url}`)}>prev</a><span>prev</span></li>
+                    <li onMouseEnter={mouseOn.bind(this)} onMouseLeave={mouseOut.bind(this)} ref={(e) => {e !== null && footerLinks.push(e)}}><a onClick={e => changePage(e, `/works/${nextUrl[0].url}`)}>next</a><span>next</span></li>
+                </ul>
+            )
+        }
+    }
     return(
         <>
         <FirstConatiner ref={firstContainer}>
-            <WorkImg ref={mainWorkImg} className='workImg'><img src={workD} /></WorkImg>
+            <WorkImg ref={mainWorkImg} ><img src={imgUrl} alt={title} /></WorkImg>
             <MainInnerConatiner>
-                <Title><span ref={mainTitle}>Soccer Meet</span></Title>
-                <WorkDescription>
-                    <Text><span ref={mainText}>Designing and planning project goals. Managing task delegation within team. Meeting with clients to discuss about client needs. Designing and creating an interface for JK Excellence to market tutoring service for prospective students.</span></Text>
-                </WorkDescription>
+            <Title><span ref={mainTitle}>{title}</span></Title>
             </MainInnerConatiner>
-            <WorkButton ref={viewButton}><img ref={workImg} src={workButton}/></WorkButton>
+            <ScrollDown ref={scrollDwon} href="#"><span></span></ScrollDown>
         </FirstConatiner>
         <SecondConatiner>
-            <InnerConatiner ref={secondContainer}>
+            <InnerConatiner ref={secondContainer} style={{backgroundColor:bgColor}}>
                 <ul>
                     <li>
                         <ul>
-                            <li><span ref={(e) => {category.push(e)}}>Concept</span></li>
-                            <li><span ref={(e) => {category.push(e)}}>UX</span></li>
-                            <li><span ref={(e) => {category.push(e)}}>WEB DEVELOPMENT</span></li>
-                            <li><span ref={(e) => {category.push(e)}}>BACK-END</span></li>
+                            <li><span ref={(e) => {e !== null && category.push(e)}}>{date}</span></li>
+                            <li><span ref={(e) => {e !== null && category.push(e)}}>{develop}</span></li>
+                            <li><span ref={(e) => {e !== null && category.push(e)}}>{useProgram}</span></li>
+                            <li><span ref={(e) => {e !== null && category.push(e)}}>{project}</span></li>
+                            <li>
+                                <ul>
+                                    {viewLink && <li><a href={viewLink} target='_blank'><span ref={(e) => {e !== null && category.push(e)}}>Visit the website</span></a></li>}
+                                    {appleStore && <li><a href={viewLink} target='_blank'><span ref={(e) => {e !== null && category.push(e)}}>Visit the website</span></a></li>}
+                                    {googlePlay && <li><a href={viewLink} target='_blank'><span ref={(e) => {e !== null && category.push(e)}}>Visit the website</span></a></li>}
+                                </ul>
+                            </li>
                         </ul>
                     </li>
                     <li>
-                        <Text ref={(e) => {category.push(e)}}>I am excited to be applying for the Front-End Engineer (Entry-Level) at Engine Digital. I am an experienced developer from Japan who studied Web Development at Cornerstone International Community College of Canada. I believe that my extensive experience in the field makes me an ideal candidate for the position at Engine Digital.</Text>
+                        <Text><span ref={projectDescription}>{description}</span></Text>
                     </li>
                 </ul>
-                <div><img ref={secondWorkImg} className='workImg' src={workD}/></div>
             </InnerConatiner>
             <InnerConatiner ref={thirdContainer}>
-                <div><img ref={thirdWorkImg} className='workImg' src={workD02}/></div>
+                <ProjectImgDiv ref={e => {e !== null && projectsImgDiv.push(e)}}><div ref={e => {e !== null && projectsImg.push(e)}}></div><img src={projectImg01} alt={title}/></ProjectImgDiv>
+                <ProjectImgDiv ref={e => {e !== null && projectsImgDiv.push(e)}}><div ref={e => {e !== null && projectsImg.push(e)}}></div><img src={projectImg02} alt={title}/></ProjectImgDiv>
+                <ProjectImgDiv ref={e => {e !== null && projectsImgDiv.push(e)}}><div ref={e => {e !== null && projectsImg.push(e)}}></div><img src={projectImg03} alt={title}/></ProjectImgDiv>
+                {projectImg04 && <ProjectImgDiv className='last-project-img' ref={e => {e !== null && projectsImgDiv.push(e)}}><div ref={e => {e !== null && projectsImg.push(e)}}></div><img src={projectImg04} alt={title}/></ProjectImgDiv>}
             </InnerConatiner>
-            <FooterNav >
-                <ul>
-                    <li ref={(e) => {footerLinks.push(e)}}><Link to=''>prev</Link><span>prev</span></li>
-                    <li ref={(e) => {footerLinks.push(e)}}><Link to=''>next</Link><span>next</span></li>
-                </ul>
-            </FooterNav>
         </SecondConatiner>
+        <FooterNav ref={footerNav}>
+            {pageNation()}
+        </FooterNav>
         </>
     )
 };
--- src/components/work-detail/work-detail.style.js
+++ src/components/work-detail/work-detail.style.js
@@ -10,37 +10,28 @@ const textMain = `
         }
     }
     h1 {
-        height: 11rem;
-        margin: auto;
-        position: fixed;
-        top: 0;
-        right: -10rem;
-        bottom: 0;
-        transform: rotate(-90deg);
+        position: absolute;
+        bottom: 15rem;
         span {
-            color:${colors.colorWh};
             letter-spacing: unset;
         }
-        @media screen and (max-width: 768px) {
-            margin-top: 5rem;
-            position: unset;
-            transform: unset;
+        @media screen and (max-width:468px) {
+            margin: auto;
+            right: 0;
+            left: 0;
+            bottom: 35rem;
             text-align: center;
-        } 
-    }
-    p {
-        span {
-            text-transform: none;
+            z-index:200;
         }
     }
 `;
 
 const textAnimation = `
-    span, p {
+    span {
         display: block;
         transform: translateY(100%);
         color: ${colors.colorWh};
-    }s
+    }
 `;
 
 export const Container = styled.div`
@@ -49,7 +40,6 @@ export const Container = styled.div`
 `;
 
 export const MainInnerConatiner = styled.div`
-    position: relative;
     padding: 16rem 4rem 0;
 `;
 
@@ -64,7 +54,7 @@ export const FirstConatiner = styled.section`
     left: 0;
     right: 0;
     margin: auto;
-    z-index:1;
+    z-index:-1;
     ${textMain};
     p {
         max-width: 700px;
@@ -89,7 +79,7 @@ export const WorkDescription = styled.div`
     position: fixed;
     left: 0;
     bottom: 2rem;
-`
+`;
 
 export const WorkImg = styled.div`
     width: 100%;
@@ -103,15 +93,13 @@ export const WorkImg = styled.div`
     bottom: 0;
     overflow: hidden;
     z-index: 101;
-    @media screen and (max-width: 768px) {
-        display: flex;
-        align-items: center;
-    }
+    display: flex;
+    align-items: center;
+    justify-content: center;
     img {
         width: auto;
-        height: 50rem;
-        @media screen and (max-width: 768px) {
-            width: 100%;
+        @media screen and (max-width: 468px) {
+            width: 60%;
             height: auto;
         }
     }
@@ -122,20 +110,17 @@ export const SecondConatiner = styled.section`
     position: relative;
     > div {
         background-color: #367E7B;
-        div {
-            overflow: hidden;
-            img {
-                transform: translateY(100%);
-            }
-        }
         ul {
             display: flex;
             justify-content: space-between;
-            @media screen and (max-width:768px) {
+            @media screen and (max-width:1024px) {
                 flex-direction: column;
             }
             ${textAnimation};
-            > li {
+            li:first-child {
+                width: 40rem;
+            }
+            li {
                 overflow: hidden;
             }
             li, p {
@@ -143,7 +128,12 @@ export const SecondConatiner = styled.section`
                 color: white;
             }
             p {
+                margin-top: -1rem;
+                overflow: hidden;
                 line-height: 2;
+                span {
+                    text-transform: unset;
+                }
             }
             > li:last-child {
                 max-width:72rem;
@@ -152,8 +142,17 @@ export const SecondConatiner = styled.section`
                 display: block;
                 margin-bottom: 5rem;
                 li {
-                    margin-top: 5rem;
+                    margin-top: 3rem;
+                    line-height: 1.6;
                     &:first-child {
+                        margin-top: 0;
+                    }
+                }
+                ul {
+                    margin-top: 5rem;
+                    display: flex;
+                    transform: translateY(100%);
+                    li {
                         margin-top: 0;
                     }
                 }
@@ -162,6 +161,42 @@ export const SecondConatiner = styled.section`
     }
     > div:first-child + div {
         background-color: white;
+    }
+`;
+
+export const ProjectImgDiv = styled.div`
+    position: relative;
+    overflow: hidden;
+    margin-bottom: 20rem;
+    &:nth-child(even) {
+        text-align: right;
+        div {
+            right:0;
+            left:unset;
+        }
+    }
+    &.last-project-img {
+        text-align: center;
+        div {
+            margin:auto;
+            right:0;
+            left:0; 
+        }
+        img {
+            box-shadow: unset;
+        }
+    }
+    div {
+        width:100%;
+        height: 1000rem;
+        max-width: 80rem;
+        position: absolute;
+        top:0;
+        left:0;
+        background-color: ${colors.mainRedColor};
+    }
+    img {
+        max-width: 80rem;
     }
 `;
 
@@ -184,7 +219,10 @@ export const FooterNav = styled.nav`
                 font-size: 15rem;
                 letter-spacing: 0;
                 color: ${colors.colorWh};
-                text-shadow: -2px -2px 0 ${colors.mainRedColor}, 2px -2px 0 ${colors.mainRedColor}, -2px 2px 0 ${colors.mainRedColor}, 2px 2px 0 ${colors.mainRedColor};
+                text-shadow:2px 0 0 ${colors.mainRedColor}, 2px 2px 0 ${colors.mainRedColor}, 0 2px 0 ${colors.mainRedColor}, -2px 2px 0 ${colors.mainRedColor}, -2px 0 0 ${colors.mainRedColor}, -2px -2px 0 ${colors.mainRedColor}, 0 -2px 0 ${colors.mainRedColor}, 2px -2px 0 ${colors.mainRedColor};
+                @media screen and (max-width:468px) {
+                    text-shadow:1px 0 0 ${colors.mainRedColor}, 1px 1px 0 ${colors.mainRedColor}, 0 1px 0 ${colors.mainRedColor}, -1px 1px 0 ${colors.mainRedColor}, -1px 0 0 ${colors.mainRedColor}, -1px -1px 0 ${colors.mainRedColor}, 0 -1px 0 ${colors.mainRedColor}, 1px -1px 0 ${colors.mainRedColor};
+                }
                 position: relative;
                 z-index:1;
             }
@@ -194,7 +232,7 @@ export const FooterNav = styled.nav`
                 position: absolute;
                 top: 0;
                 bottom: 0;
-                font-size: 60rem;
+                font-size: 50rem;
                 opacity:0;
                 animation-name: marquee;
                 animation-duration: 20s;
@@ -212,16 +250,14 @@ export const FooterNav = styled.nav`
                 border-right: 1px solid ${colors.mainRedColor};
             }
         }
+        .active-link {
+            width: 100vw!important;
+            span {
+                padding-left: 30rem;
+            }
+            a {
+                border-right: none!important;
+            }
+        }
     }
-`;
-
-export const WorkButton = styled.a`
-    width: 20rem;
-    height: 10rem;
-    position: fixed;
-    right: 8rem;
-    bottom: 15rem;
-    margin: auto;
-    display: block;
-    opacity:0;
 `;
