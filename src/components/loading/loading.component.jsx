1b3f32eb117f406966d29278fb48f1d726ad5de7
yasuhiro <yasucom4646@gmail.com>
2020-03-23 04:14:27 -0700

    Added title animation

--- src/components/loading/loading.component.jsx
+++ src/components/loading/loading.component.jsx
@@ -4,11 +4,21 @@ import { TweenLite, Expo } from 'gsap';
 
 const Loading = () => {
     const loadingOverlay = useRef(null);
+    const title = useRef(null);
     useEffect(() => {
         TweenLite.set(loadingOverlay.current, { height: '100vh' });
-        TweenLite.to(loadingOverlay.current, 1.5, {
+        TweenLite.to(loadingOverlay.current, 1, {
+            delay: 0.5,
             height: 0,
             ease: Expo.easeInOut
+          });
+          TweenLite.to(title.current, 3, {
+            delay: 1.7,
+            height:'200px',
+            fontSize: '15.2rem',
+            scale: 10,
+            ease: Expo.easeInOut,
+            opacity: 0
           });
     })
     return(
