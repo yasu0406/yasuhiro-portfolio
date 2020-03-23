8e1b180604dfeb24ae9adc9440fe22aca4325b1a
yasuhiro <yasucom4646@gmail.com>
2020-03-23 04:14:08 -0700

    Created header compoennt

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,17 +1,130 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import {Header, Logo, HeadrNav} from './header.style';
+import React, { useEffect, useState, useRef } from 'react';
+import { useHistory } from "react-router-dom";
+import { TweenLite, Expo } from 'gsap';
+
+import {Header, Logo, HeaderMenu, Hamburger, NavLink, SnsLink} from './header.style';
 
 const HeaderComponent = () => {
+    const hamburger = useRef(null);
+    const menu = useRef(null);
+    const logo = useRef(null);
+    let navList = [];
+    let bars = [];
+    const [menuToggle, setMenuToggle] = useState(true);
+    let history = useHistory();
+    useEffect(() => {
+        var aList = document.querySelectorAll('a');
+        for(var i = 0; i < aList.length; i++) {
+            aList[i].addEventListener('click', onCloseMenu);
+        }
+        return () => {
+            window.removeEventListener('click', onClickMenu);
+        }
+    });
+    const changePage = (e, destination) => {
+        e.preventDefault();
+        onCloseMenu();
+        setTimeout(() => {
+          history.push(destination);
+        }, 950);
+    };
+    const onClickMenu = () => {
+        setMenuToggle(!menuToggle);
+        if(menuToggle === true) {
+            TweenLite.to(menu.current, 1, {
+                width: '100%',
+                ease: Expo.easeInOut
+            });
+            TweenLite.to(bars[0], 0.5, {
+                transform: 'rotate(48deg) translateY(7px) translateX(9px)',
+                backgroundColor:'white'
+            });
+            TweenLite.to(bars[1], 0.5, {
+                transform: 'rotate(-48deg)',
+                backgroundColor:'white'
+            });
+            TweenLite.to(bars[2], 0.5, {
+                opacity: 0
+            });
+            TweenLite.to(logo.current.querySelector('a'), 0.5, {
+                color: 'white'
+            });
+            TweenLite.to(navList[0], 0.5, {
+                delay: 1,
+                transform: 'translateY(0)'
+            });
+            TweenLite.to(navList[1], 0.5, {
+                delay: 1.3,
+                transform: 'translateY(0)'
+            });
+            // TweenLite.to(navList[2], 0.5, {
+            //     delay: 1.6,
+            //     transform: 'translateY(0)'
+            // });
+        } else {
+            onCloseMenu();
+        }        
+    }
+    const onCloseMenu = () => {
+        setMenuToggle(true);
+        TweenLite.to(navList[0], 0.5, {
+            transform: 'translateY(100%)'
+        });
+        TweenLite.to(navList[1], 0.5, {
+            delay: 0.3,
+            transform: 'translateY(100%)'
+        });
+        // TweenLite.to(navList[2], 0.5, {
+        //     delay: 0.6,
+        //     transform: 'translateY(100%)'
+        // });
+        TweenLite.to(menu.current, 0.5, {
+            delay: 1,
+            width: 0,
+            ease: Expo.easeInOut
+        });
+        TweenLite.to(bars[0], 0.5, {
+            delay: 1,
+            transform: 'unset',
+            backgroundColor:'#E33642'
+        });
+        TweenLite.to(bars[1], 0.5, {
+            delay: 1,
+            transform: 'unset',
+            backgroundColor:'#E33642'
+        });
+        TweenLite.to(bars[2], 0.5, {
+            delay: 1,
+            opacity: 1
+        });
+        TweenLite.to(logo.current.querySelector('a'), 0.5, {
+            delay: 1,
+            color: '#E33642'
+        });
+    }
     return (
         <Header>
-            <Logo><Link to='/'>yasuhiro<br />katayama</Link></Logo>
-            <HeadrNav>
-                <ul>
-                    <li><Link to='/profile'>profile</Link></li>
-                    <li><Link>contact</Link></li>
-                </ul>
-            </HeadrNav>
+            <Logo ref={logo}><a onClick={e => changePage(e, `/`)}>yasuhiro<br />katayama</a></Logo>
+            <div>
+                <Hamburger className='hamburger' onClick={() => {onClickMenu()}} ref={hamburger}>
+                    <span ref={(e) => { e !== null && bars.push(e) }}></span>
+                    <span ref={(e) => { e !== null && bars.push(e) }}></span>
+                    <span ref={(e) => { e !== null && bars.push(e) }}></span>
+                </Hamburger>
+                
+                <HeaderMenu ref={menu}>
+                    <menu>
+                        <li><NavLink onClick={e => changePage(e, `/works`)} ref={(e) => { e !== null && navList.push(e) }}><span data-text="works">works</span></NavLink></li>
+                        <li><NavLink onClick={e => changePage(e, `/profile`)} ref={(e) => { e !== null && navList.push(e) }}><span data-text="profile">profile</span></NavLink></li>
+                        {/* <li><NavLink onClick={e => changePage(e, `/profile`)} ref={(e) => { e !== null && navList.push(e) }}><span data-text="contact">contact</span></NavLink></li> */}
+                    </menu>
+                </HeaderMenu>
+            </div>
+            <SnsLink>
+                <li><a href='https://www.linkedin.com/in/yasuhiro-katayama-118423160/' target='_blank'>linkedin</a></li>
+                <li><a href='https://github.com/yasu0406' target='_blank'>github</a></li>
+                <li><a href='https://yasuhiro-k.com/images/resume.pdf' target='_blank'>resume</a></li>
+            </SnsLink>
         </Header>
     )
 }
--- src/components/header/header.style.js
+++ src/components/header/header.style.js
@@ -1,31 +1,123 @@
 import styled from 'styled-components';
+import { colors } from '../../styles/variable';
 
 export const Header = styled.header`
+    width: calc(100% - 8rem);
     padding: 4rem 4rem 2.5rem 4rem;
     display: flex;
     justify-content: space-between;
     align-items: center;
-    @media screen and (max-width: 768px) {
-        padding: 2rem 2rem 1.25rem 2rem;
+    position: fixed;
+    top: 0;
+    z-index:103;
+    @media screen and (max-width: 1024px) {
+        padding: 2rem 4rem 2.5rem 4rem;
+    }
+    @media screen and (max-width: 468px) {
+        padding: 6rem 4rem 2.5rem 4rem;
     }
 `;
 
 export const Logo = styled.h1`
+    z-index: 10;
     a {        
         font-size: 3.2rem;
-        @media screen and (max-width:768px) {
-            & {
-                font-size: 1.7rem;
-            }
-        }
     }
 `;
 
-export const HeadrNav = styled.nav`
-    ul {
-        display: flex;
-        li:first-child {
-            margin-right:4.5rem;
+export const HeaderMenu = styled.nav`
+    position:fixed;
+    top: 0;
+    right: 0;
+    width:0;
+    height:100vh;
+    background-color:#243563;
+    overflow-y:scroll;
+    padding-bottom:30px;
+    display: flex;
+    align-items: center;
+    menu {
+        margin:60px auto;
+        position:relative;
+        text-align:center;
+        li {
+            margin-top: 2rem;
+            overflow: hidden;
+            &:first-child {
+                margin-top: 2rem;
+            }
+        }
+        a {
+            transform: translateY(100%);
+            display: block;
+        }
+        span {
+            position: relative;
+            color: #243563;
+          }
+          
+        span:before {
+            width: 0;
+            color: ${colors.colorWh};
+            overflow: hidden;
+            position: absolute;
+            content: attr(data-text);
+            transition: all 1s cubic-bezier(0.84, 0, 0.08, 0.99);
+          }
+          
+         span:hover:before {
+            width: 100%;
+          }
+    }
+`;
+
+export const Hamburger = styled.div`
+    position: fixed;
+    padding:10px;
+    top:2rem;
+    right:2rem;
+    width:6.5rem;
+    height:5.0rem;
+    z-index:10;
+    transition:0.3s;
+    animation:moveme 0.3s 2 alternate backwards;
+    &:hover {
+        width:8rem;
+    }
+    span {
+        width:100%;
+        height:3px;
+        background-color:${colors.mainRedColor};
+        display:block;
+        margin:10px auto;
+    }
+    @keyframes moveme {
+        from {width: 65px;}
+        to {width: 75px;}
+    }
+`;
+
+export const NavLink = styled.a`
+    position: relative;
+    font-size: 12rem;
+    letter-spacing: 0;
+    color: ${colors.mainRedColor};
+    text-shadow:2px 0 0 ${colors.mainRedColor}, 2px 2px 0 ${colors.mainRedColor}, 0 2px 0 ${colors.mainRedColor}, -2px 2px 0 ${colors.mainRedColor}, -2px 0 0 ${colors.mainRedColor}, -2px -2px 0 ${colors.mainRedColor}, 0 -2px 0 ${colors.mainRedColor}, 2px -2px 0 ${colors.mainRedColor};
+    @media screen and (max-width:468px) {
+        text-shadow:1px 0 0 ${colors.mainRedColor}, 1px 1px 0 ${colors.mainRedColor}, 0 1px 0 ${colors.mainRedColor}, -1px 1px 0 ${colors.mainRedColor}, -1px 0 0 ${colors.mainRedColor}, -1px -1px 0 ${colors.mainRedColor}, 0 -1px 0 ${colors.mainRedColor}, 1px -1px 0 ${colors.mainRedColor};
+    }
+`;
+
+export const SnsLink = styled.ul`
+    position: fixed;
+    right: 4rem;
+    bottom: 4rem;
+    display: flex;
+    li {
+        margin-left: 2rem;
+        font-size: 1rem;
+        &:first-child {
+            margin-left: none;
         }
     }
 `;
