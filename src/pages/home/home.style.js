a9609a2a4c64b0e6750b9f9ec3958b3c73e93029
yasuhiro <yasucom4646@gmail.com>
2020-03-23 04:19:33 -0700

    Update home.style.js

--- src/pages/home/home.style.js
+++ src/pages/home/home.style.js
@@ -1,17 +1,17 @@
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { colors } from '../../styles/variable';
+
+export const Container = styled.div`
+    height: 100vh;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
 
 export const MainUl = styled.ul`
     width: max-content;
-    height: 57rem;
     text-align: center;
-    position: absolute;
-    top: 0;
-    right: 0;
-    left: 0;
-    bottom: 0;
-    margin: auto;
-    z0index: 1;
     li {
         overflow: hidden;
         &:first-child,
@@ -19,10 +19,43 @@ export const MainUl = styled.ul`
             font-size: 4rem;
         }
         &:nth-child(2) {
+            position: relative;
+            span {
+                position: relative;
+            }
+            span:before {
+                content: '';
+                width: 88%;
+                height: 4px;
+                margin: auto;
+                background-color: ${colors.mainRedColor};
+                display: block;
+                position: absolute;
+                right: 5rem;
+                bottom: 4px;
+                transition: 1s;
+            }
+            &:hover span:before {
+                width: 0;
+            }
             a {
                 position: relative;
                 z-index: 2;
                 font-size: 12.4rem;
+            }
+            .profileImgLink {
+                width:0;
+                max-width:40rem;
+                height: 40rem;
+                position: fixed;
+                overflow: hidden;
+                z-index: 100;
+                img {
+                    width:40rem;
+                }
+                @media screen and (max-width:1024px) {
+                    display:none;
+                }
             }
         }
         &:nth-child(3) {
@@ -41,61 +74,37 @@ export const MainUl = styled.ul`
         @media screen and (min-width:1088px) {
             line-height:1.22;
                 &:nth-child(2) {
-                    line-height:.86;
+                    line-height:.96;
                 }
                 &:nth-child(3) {
-                    line-height: .91;
+                    line-height: 1.01;
                 }
                 &:nth-child(4) {
-                    line-height: 1.15;
+                    line-height: 1.25;
                 }
                 &:nth-child(5) {
                     margin-top: -3.3rem;
                 }
                 &:last-child {
-                    margin-top: -3.2rem;
+                    margin-top: -2rem;
                 }
         }
-        @media screen and (max-width:768px) {
-            &:first-child,
-            &:last-child {
-                font-size: 2rem;
-            }
-            &:nth-child(2) {
-                a {
-                    font-size: 6.2rem;
-                }
-            }
-            &:nth-child(3) {
-                font-size: 3.2rem;
-            }
-            &:nth-child(4) {
-                font-size: 2.6rem;
-                span {
-                    margin-top: -1rem;
-                    font-size: 8.2rem;
-                }
-            }
-        }
-    }
-    @media screen and (min-height:801px) {
-        bottom: -10rem;
-    }
-    @media screen and (max-width:768px) {
-        bottom:0;
     }
 `;
 
-export const WorkButton = styled(Link)`
+export const WorkButton = styled.a`
+    width: 20rem;
     position: fixed;
     left: 0;
     right: 0;
     top: -9rem;
     margin: auto;
-    width: 200px;
-    z-index: 1;
     display: block;
     img {
         height: 15rem;
+    }
+    @media screen and (max-width:1088px) {
+        top:unset;
+        bottom:5rem;
     }
 `;
